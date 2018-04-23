8f306d997f827e98ef1278cd566ebe65d103a5b6
Precidia <precidia.tom@gmail.com>
2018-04-23 12:49:59 -0700

    Added var port for app.listen

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const APP_KEY = 'df03da67ec2c0fb66e7628b0c84c9bec';
 var app = express();
 var resultRecipes = '';
 var loggedIn = false;
+var port = process.env.PORT || 8000;
 
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials');
@@ -24,7 +25,7 @@ hbs.registerHelper('getCopyRights', () => {
     return "Rest in Pepperoni";
 });
 
-app.get('/', (request, response) => {
+app.get('/getpass', (request, response) => {
     response.render('login.hbs')
 });
 
@@ -134,6 +135,9 @@ function checkRecords() {
     }
 }
 
-app.listen(process.env.PORT || 8000, () => {
-    console.log('Server is up on the port 8081');
+function checkInputs(user, password) {
+}
+
+app.listen(port, () => {
+    console.log('Server is up on the port 8080');
 });
