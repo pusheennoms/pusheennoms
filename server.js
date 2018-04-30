8ccec27dba0fbb6ce3accb39534eed830cc96f19
emilieez <emilie.m.zhang@gmail.com>
2018-04-30 10:56:50 -0700

    recipe router works

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require('body-parser');
 
 var app = express();
 var loginRouter = require('./routers/login');
-var resultRecipes = '';
+var homeRouter = require('./routers/home');
 
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
@@ -17,26 +17,28 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use('/', loginRouter);
+app.use('/search', homeRouter);
 
-/**
- * Controller for queries through the address bar
- **/
-app.get('/search', function (req, res, next) {
-    utils.getRecipes(req.query, (error, results) => {
-        resultRecipes = JSON.stringify(results.recipes);
-        res.render('home.hbs', {
-            resultRecipes: resultRecipes
-        });
-    });
-});
-
-/**
- * The action to download a recipe
- **/
-app.post('/download', function (req, res) {
-    var recipe = JSON.parse(req.body.recipe);
-    fs.writeFileSync(recipe.label + '.txt', req.body.recipe);
-});
+//
+// /**
+//  * Controller for queries through the address bar
+//  **/
+// app.get('/search', function (req, res, next) {
+//     utils.getRecipes(req.query, (error, results) => {
+//         resultRecipes = JSON.stringify(results.recipes);
+//         res.render('home.hbs', {
+//             resultRecipes: resultRecipes
+//         });
+//     });
+// });
+//
+// /**
+//  * The action to download a recipe
+//  **/
+// app.post('/download', function (req, res) {
+//     var recipe = JSON.parse(req.body.recipe);
+//     fs.writeFileSync(recipe.label + '.txt', req.body.recipe);
+// });
 
 // /**
 //  * After adding a new chef to the chef file, redirects to home page
