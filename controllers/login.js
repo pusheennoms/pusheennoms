3bf30de257154d3f0aa728b8de60ba4d934334b3
emilieez <emilie.m.zhang@gmail.com>
2018-05-01 10:19:01 -0700

    Notifying user on valid/invalid account creation works

--- controllers/login.js
+++ controllers/login.js
@@ -13,13 +13,13 @@ router.get('/', function (req, res, next) {
 router.post('/registerchef', (request, response) => {
     var valid = utils.validateInput(request.body.username, request.body.password);
 
-    if (valid == true) {
+    if (valid === true) {
         utils.addToChefFile(request.body.username, request.body.password);
         response.render('login.hbs', {
             status: true
         });
     }
-    else if (valid == false) {
+    else if (valid === false) {
         response.render('login.hbs', {
             status: false
         });
