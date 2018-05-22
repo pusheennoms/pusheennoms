52ce6620d515c083cd0e44d4d195600d137e761f
nathankazuki <35462812+nathankazuki@users.noreply.github.com>
2018-05-22 16:55:21 -0700

    Update www.js

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 var app = require('../app');
 
-app.listen(process.env.PORT || 8010, () => {
-    console.log('Server is up on the port 8010');
+app.listen(process.env.PORT || 8008, () => {
+    console.log('Server is up on the port 8008');
 });
